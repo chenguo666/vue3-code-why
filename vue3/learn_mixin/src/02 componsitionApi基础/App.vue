43cecd2c7e6446bdd1ec5a93a4bfdc730fba8dbb
fruitchan <1305378470@qq.com>
2021-10-18 17:13:04 +0800

    componsition api 其他api

--- vue3/learn_mixin/src/02 componsitionApi基础/App.vue
+++ vue3/learn_mixin/src/02 componsitionApi基础/App.vue
@@ -13,6 +13,13 @@ import { reactive, ref, readonly } from 'vue'
 export default {
   // 有哪些参数 props 父组件传递过来的属性
   //有什么样的返回值
+
+  //   isProxy 检查对象是不是由reactive或readonly创建的proxy
+  //   isReactive 检查对象是不是由reactive创建的响应代理
+  //   isReadonly
+  //   toRaw 返回reactive或readonly代理的原始对象 谨慎使用
+  //   shallowReactive 创建一个响应式代理 它跟踪其自身property的响应性
+  //   shallowReadonly
   setup(props, context) {
     console.log(props, context.attrs)
     // let counter = 100

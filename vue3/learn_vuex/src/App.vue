608b2c24f95b6a9929dec968dbcc67f30db9c462
fruitchan <1305378470@qq.com>
2021-11-08 14:25:45 +0800

    vue3 vuex

---
+++ vue3/learn_vuex/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div>
+    {{ counter }}--{{ $store.state.counter }}
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</template>
+
+<script>
+  // import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    name: 'App',
+    data() {
+      return {
+        counter: 0
+      }
+    },
+    methods: {
+      increment() {
+        this.$store.commit('increment')
+      },
+      decrement() {
+        this.$store.commit('decrement')
+      }
+    }
+    //   components: {
+    //     HelloWorld
+    //   }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>

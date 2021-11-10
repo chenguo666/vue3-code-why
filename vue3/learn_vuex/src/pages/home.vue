54b85802af42660639e1fc650c6409e80058c3cc
fruitchan <1305378470@qq.com>
2021-11-10 11:04:45 +0800

    vuex 辅助函数 mapMutations

--- vue3/learn_vuex/src/pages/home.vue
+++ vue3/learn_vuex/src/pages/home.vue
@@ -1,14 +1,13 @@
 <template>
   <h1>home:{{ $store.state.counter }}</h1>
   <br />
-  <button @click="$store.commit('increment')">+</button>
-  <button @click="$store.commit('incrementn', 10)">+10</button>
-  <button @click="addH">+100</button>
-  <button @click="$store.commit('decrement')">-</button>
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
 </template>
 <script>
   import {
-    useStore
+    useStore,
+    mapMutations
   } from 'vuex'
   import {
     INCREMENT_N
@@ -16,11 +15,9 @@
   export default {
     setup() {
       const store = useStore()
-      const addH = () => {
-        store.commit(INCREMENT_N, 100)
-      }
+      const storeMutations = mapMutations(['increment', 'decrement'])
       return {
-        addH
+        ...storeMutations
       }
     }
   }
---
+++ vue3/learn_vuex/src/pages/mutations细节补充.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>home:{{ $store.state.counter }}</h1>
+  <br />
+  <button @click="$store.commit('increment')">+</button>
+  <button @click="$store.commit('incrementn', 10)">+10</button>
+  <button @click="addH">+100</button>
+  <button @click="$store.commit('decrement')">-</button>
+</template>
+<script>
+  import {
+    useStore
+  } from 'vuex'
+  import {
+    INCREMENT_N
+  } from '../store/mutations-types.js'
+  export default {
+    setup() {
+      const store = useStore()
+      const addH = () => {
+        store.commit(INCREMENT_N, 100)
+      }
+      return {
+        addH
+      }
+    }
+  }
+</script>
+<style></style>

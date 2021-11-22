4f3ab57ed7432291aedbedc82fc38d2dd0e52dfa
fruitchan <1305378470@qq.com>
2021-11-22 16:52:15 +0800

    ts 模块

--- ts/03 webpack ts/src/main.ts
+++ ts/03 webpack ts/src/main.ts
@@ -1,5 +1,9 @@
 import {add,sub} from './utils/math'
 import {time,price} from './utils/format'
+import axios from 'axios'
+// npm i @types/lodash -D
+// 或者自己编写声明文件
+import lodash from 'lodash'
 // 类型注解
 const message: string = 'xfasdf'
 console.log(message)
@@ -8,4 +12,8 @@ console.log(9999)
 console.log(add(10,10));
 console.log(sub(10,10));
 console.log(time.format('xxx'));
+axios.get('http://123.207.32.32:8000/home/multidata').then(res=>{
+    console.log(res);
+    
+})
 

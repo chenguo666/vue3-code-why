b2eb60c7d458e52086e59d4acf9214a16ef02c07
fruitchan <1305378470@qq.com>
2021-11-24 15:09:37 +0800

    项目格式化 prettier

--- 实战/vue3-ts-cms/src/App.vue
+++ 实战/vue3-ts-cms/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/"> Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
@@ -17,11 +17,9 @@
 
 #nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
-
     &.router-link-exact-active {
       color: #42b983;
     }
--- 实战/vue3-ts-cms/src/views/Home.vue
+++ 实战/vue3-ts-cms/src/views/Home.vue
@@ -6,13 +6,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
-export default defineComponent({
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-});
+  import {
+    defineComponent
+  } from 'vue';
+  import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  export default defineComponent({
+    name: 'Home',
+    components: {
+      HelloWorld
+    }
+  });
 </script>
